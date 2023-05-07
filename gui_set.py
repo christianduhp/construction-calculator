31cd4b2d03f9b71d170743b5deaf146d6224e31d
Christian Oliveira <christianoliver2010@gmail.com>
2023-05-06 22:56:47 -0300

    Improving code organization into modules
    In this update, we simplified the code by removing any redundant blocks and lines of code. The simplification process reduced the overall complexity of the code, making it more efficient and easier to maintain.

---
+++ gui_set.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter.font import Font
+
+janela = Tk()
+janela.title('Calculadora Primos Depósito')
+janela.geometry('490x560+500+80')
+photo = PhotoImage(file = 'images//icon.png')
+janela.wm_iconphoto(False, photo)
+janela.resizable(0, 0)
+
+img_bg_main = PhotoImage(file='images//img_bg_main.png')
+img_bg_forro = PhotoImage(file='images//img_bg_forro.png')
+img_bg_laje = PhotoImage(file='images//img_bg_laje.png')
+img_bg_forro = PhotoImage(file='images//img_bg_forro.png')
+img_bg_opcao_laje = PhotoImage(file='images//img_bg_opcao_laje.png')
+img_bg_area_laje = PhotoImage(file='images//img_bg_area_laje.png')
+img_button_calcular_forro = PhotoImage(file='images//button_calcular_forro.png')
+img_button_calcular_laje = PhotoImage(file='images//button_calcular_laje.png')
+img_button_tenho_trilho = PhotoImage(file='images//button_tenho_trilho.png')
+img_button_nao_tenho_trilho = PhotoImage(file='images//button_nao_tenho_trilho.png')
+img_button_calcular = PhotoImage(file='images//button_calcular.png')
+img_button_back = PhotoImage(file='images//button_back.png')
+img_button_info = PhotoImage(file='images//button_info.png')
+img_button_trilho_comprimento = PhotoImage(file='images//button_trilho_comprimento.png')
+img_button_trilho_largura = PhotoImage(file='images//button_trilho_largura.png')
+
